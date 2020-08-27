7b2451d20ccff978c6c445d9cc221d856050d120
Ubuntu <ju-ucsb@grafter-example.csia4dhhuauuniqhkqjlxq0owf.dx.internal.cloudapp.net>
2020-08-27 08:18:26 +0000

    m

--- benchmarks/grafter/OOPSLAExample/UNFUSED/main2_ag.cpp
+++ benchmarks/grafter/OOPSLAExample/UNFUSED/main2_ag.cpp
@@ -1,3 +1,78 @@
+#include <assert.h>
+#include <chrono>
+#include <stddef.h>
+#include <stdio.h>
+
+#define __tree_structure__ __attribute__((annotate("tf_tree")))
+#define __tree_child__ __attribute__((annotate("tf_child")))
+#define __tree_traversal__ __attribute__((annotate("tf_fuse")))
+
+enum NodeType {
+	VAL_NODE,
+	NULL_NODE
+};
+
+class __tree_structure__ Node {
+	public: 
+		NodeType Type;
+		__tree_traversal__ virtual void f1() {}
+		__tree_traversal__ virtual void f2() {}
+};
+
+class __tree_structure__ NullNode: public Node {
+	__tree_traversal__ void f1() { return; }
+	__tree_traversal__ void f2() { return; }
+};
+
+class __tree_structure__ ValueNode: public Node {
+	public: 
+		int v, x, y;
+		__tree_child__ Node * Left, * Right;
+		__tree_traversal__ void f1() {
+			bool is_leaf;
+			is_leaf = (Left -> Type == NULL_NODE) && (Right -> Type == NULL_NODE);
+			Left -> f1();
+			Right -> f1();
+			if (is_leaf) {
+				v = 1;
+			} else {
+				v = static_cast < ValueNode * > (Left) -> v + static_cast < ValueNode * > (Right) -> v;
+			}
+			return;
+		}
+		__tree_traversal__ void f2() {
+			// left part
+			if (Left -> Type == VAL_NODE) {
+				ValueNode *const LL = static_cast < ValueNode * > (Left);
+				bool left_is_leaf;
+				left_is_leaf = (LL -> Left -> Type == NULL_NODE) && (LL -> Right -> Type == NULL_NODE);
+				if (left_is_leaf) {
+					LL -> x = v + 2;
+				} else {
+					LL -> x = LL -> v;
+				}
+				LL -> y = LL -> v * 2;
+			}
+			
+
+			// right part
+			if (Right -> Type == VAL_NODE) {
+				ValueNode *const RR = static_cast < ValueNode * > (Right);
+				bool right_is_leaf;
+				right_is_leaf = (RR -> Left -> Type == NULL_NODE) && (RR -> Right -> Type == NULL_NODE);
+				if (right_is_leaf) {
+					RR -> x = 9999;
+				} else {
+					RR -> x = RR -> v;
+				}
+				RR -> y = RR -> v * 2;
+			}
+			
+			Left -> f2();
+			Right -> f2();
+		}
+};
+
 Node * createTree() {
 	Node * Root = new ValueNode();
 	ValueNode *const _Root = static_cast<ValueNode *>(Root);
@@ -73,3 +148,11 @@ Node * createTree() {
 
 	return Root;
 }
+
+int main() {
+	Node * Root = createTree();
+	// here starts the part that requires fusion
+	Root -> f1();
+	Root -> f2();
+}
+
